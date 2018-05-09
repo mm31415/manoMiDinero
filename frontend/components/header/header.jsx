1d510c7ef94ca7ec7d3798d5f03ec96bb29e588c
mm31415 <martinez.m31415@gmail.com>
2018-05-09 17:52:38 -0400

    Add styling to for logged out header display

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -14,17 +14,19 @@ export const Header = (props) => {
         <Link to="/" className="homepage-link" onClick={props.resetSessionErrors}>SPLITWISECLONE</Link>
         <nav className="header-nav">
           <Link className="login-btn" to="/login" onClick={props.resetSessionErrors}>Log in</Link>
-          or
+          <strong>or</strong>
           <Link className="signup-btn" to="/signup" onClick={props.resetSessionErrors}>Sign up</Link>
         </nav>
       </nav>
     );
   } else {
     return (
-      <nav>
+      <nav className="header">
         <Link to="/" className="homepage-link" onClick={props.resetSessionErrors}>SPLITWISECLONE</Link>
-        <h2>Welcome {props.user.name}</h2>
-        <button onClick={handleLogout}>Logout</button>
+          <nav className="header-nav">
+            Welcome {props.user.name}
+            <button onClick={handleLogout}>Logout</button>
+          </nav>
       </nav>
     );
   }
