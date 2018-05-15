6b8cf8eb5b0c033f33f7e758a8027a820d855972
mm31415 <martinez.m31415@gmail.com>
2018-05-15 15:57:32 -0400

    Add checkpoint to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+User.destroy_all
 User.create(name: "guest", email: "guestemail@guest.com", password: "guestlogin");
+User.create(name: "Abby", email: "abbyemail@abby.com", password: "abbylogin");
+User.create(name: "Paul", email: "paulemail@paul.com", password: "paullogin");
+User.create(name: "Jahan", email: "jahanemail@jahan.com", password: "jahanlogin");
+User.create(name: "Alicia", email: "aliciaemail@alicia.com", password: "alicialogin");
+User.create(name: "Brian", email: "brianemail@brian.com", password: "brianlogin");
+
+Friendship.destroy_all
