45223cb807dde00e118c42b8dd32b255c89a6259
mm31415 <martinez.m31415@gmail.com>
2018-05-21 10:08:31 -0400

    Starting checkpoint for the day

--- frontend/components/info_sidebar/info_sidebar.jsx
+++ frontend/components/info_sidebar/info_sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 
 export const InfoSidebar = (props) => {
+  const friendId = props.location.pathname.split("/")[3] - 0;
 
   const handleDelete = (friendId) => {
     return e => {
@@ -11,15 +12,51 @@ export const InfoSidebar = (props) => {
   };
 
   const deleteFriendButton = () => {
-    const friendId = props.location.pathname.split("/")[3] - 0;
     if (friendId) {
       return <button onClick={handleDelete(friendId)}>Delete Friend</button>;
     }
   };
 
+  const balance = () => {
+    if (friendId) {
+      if ((props.owed - props.lent) < 0) {
+        return(
+          <div>
+            <h1>You owe {props.name}</h1>
+            <h2>${props.lent - props.owed}</h2>
+          </div>
+        );
+      } else {
+        return(
+          <div>
+            <h1>{props.name} owes you</h1>
+            <h2>${props.owed - props.lent}</h2>
+          </div>
+        );
+      }
+    } else {
+      if ((props.owed - props.lent) < 0) {
+        return(
+          <div>
+            <h1>You owe</h1>
+            <h2>${props.lent - props.owed}</h2>
+          </div>
+        );
+      } else {
+        return(
+          <div>
+            <h1>You are owed</h1>
+            <h2>${props.owed - props.lent}</h2>
+          </div>
+        );
+      }
+    }
+  };
+
   return(
     <div className="info-sidebar">
-      <h1>this is the info sidebar</h1>
+      <h1>{friendId ? "Your balance" : "Your total balance"}</h1>
+      {balance()}
       {deleteFriendButton()}
     </div>
   );
