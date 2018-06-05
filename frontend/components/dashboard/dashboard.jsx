769925eecec13c824ad9830b1acc682651d94385
mm31415 <martinez.m31415@gmail.com>
2018-06-05 15:23:22 -0400

    Add functionality to clicking on chart or list item in dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -9,6 +9,10 @@ export const Dashboard = (props) => {
     return <Redirect to="/" />;
   }
 
+  const handleClick = (e, props) => {
+    props.history.push(`/main/friends/${e.currentTarget.value}`);
+  };
+
   const totalBalance = () => {
     const amount = balance(props.expenses, props.logged_in);
     if (amount < 0) {
@@ -53,7 +57,8 @@ export const Dashboard = (props) => {
   const listItem = (amount, friendName, friendId) => {
     if (amount < 0) {
       return (
-        <li key={friendId}>
+        <li key={friendId} value={friendId}
+          onClick={(e) => handleClick(e, props)}>
           <img src={window.staticImages.avatar}></img>
           <div>
             <h1>{friendName}</h1>
@@ -63,7 +68,8 @@ export const Dashboard = (props) => {
       );
     } else {
       return (
-        <li key={friendId}>
+        <li key={friendId} value={friendId}
+          onClick={(e) => handleClick(e, props)}>
           <img src={window.staticImages.avatar}></img>
           <div>
             <h1>{friendName}</h1>
@@ -91,6 +97,70 @@ export const Dashboard = (props) => {
           {oweInfo}
         </ul>
         <ul id="lender-list">
+          {lenderInfo}
+        </ul>
+      </div>
+    );
+  };
+
+  const chartItem = (amount, friendName, friendId) => {
+    let liStyle = {};
+    let width;
+    if (amount < 0) {
+      width = parseInt(amount * -1);
+      if (width < 50) {
+        liStyle = { width: '50px' };
+      } else {
+        liStyle = { width: `${width}px` };
+      }
+      return (
+        <li id="ower-chart-li" key={friendId}
+          value={friendId} style={liStyle}
+          onClick={(e) => handleClick(e, props)}>
+          <div>
+            <h1>{friendName}</h1>
+            <h2>${(amount * -1).toFixed(2)}</h2>
+          </div>
+        </li>
+      );
+    } else {
+      width = parseInt(amount);
+      if (width < 50) {
+        liStyle = { width: '50px' };
+      } else {
+        liStyle = { width: `${width}px` };
+      }
+
+      return (
+        <li id="lender-chart-li" key={friendId}
+          value={friendId} style={liStyle}
+          onClick={(e) => handleClick(e, props)}>
+          <div>
+            <h1>{friendName}</h1>
+            <h2>${amount.toFixed(2)}</h2>
+          </div>
+        </li>
+      );
+    }
+  };
+
+  const dashChart = (props) => {
+    const oweInfo = [];
+    const lenderInfo = [];
+    props.friendIds.forEach((id) => {
+      const amount = balance(props.friendExpenses[id], props.logged_in);
+      if (amount < 0) {
+        oweInfo.push(chartItem(amount, props.users[id].name, id));
+      } else if (amount > 0) {
+        lenderInfo.push(chartItem(amount, props.users[id].name, id));
+      }
+    });
+    return (
+      <div id="dash-chart">
+        <ul id="owe-chart">
+          {oweInfo}
+        </ul>
+        <ul id="lender-chart">
           {lenderInfo}
         </ul>
       </div>
@@ -127,8 +197,10 @@ export const Dashboard = (props) => {
         <h1 id="head-right">YOU ARE OWED</h1>
       </nav>
       {dashList(props)}
+      {dashChart(props)}
     </div>
   );
+
 
 
 };
