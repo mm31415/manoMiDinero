23380f0ef69ed89f276e4437c627bafaceaff99d
mm31415 <martinez.m31415@gmail.com>
2018-06-05 10:43:35 -0400

    Add working display of amounts on dashboard, next is working on chart and list view on dashboard

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,43 +1,87 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import { balance } from "../../util/function_util";
 
 
-class Dashboard extends React.Component {
+export const Dashboard = (props) => {
 
-  render() {
-    if (!this.props.logged_in) {
-      return <Redirect to="/" />;
+  if (!props.logged_in) {
+    return <Redirect to="/" />;
+  }
+
+  const totalBalance = () => {
+    const amount = balance(props.expenses, props.logged_in);
+    if (amount < 0) {
+      return <h2 id="ower">- ${(amount * -1).toFixed(2)}</h2>;
+    } else {
+      return <h2 id="lender">+ ${amount.toFixed(2)}</h2>;
     }
+  };
 
-    return (
-      <div className="dashboard">
-        <ul id="balances">
-          <li>
-            <h1>total balance</h1>
+  const oweBalance = () => {
+    const oweExpenses = [];
+    for (let i = 0; i < props.expenses.length; i++) {
+      const expense = props.expenses[i];
+      if (expense.payee_id !== undefined && expense.payee_id !== props.logged_in) {
+        oweExpenses.push(expense);
+      } else if (expense.splits !== undefined && expense.payer_id !== props.logged_in) {
+        oweExpenses.push(expense);
+      }
+    }
+    let amount = balance(oweExpenses, props.logged_in);
+    if (amount < 0) {
+      return <h2 id="ower">${(amount * -1).toFixed(2)}</h2>;
+    } else {
+      return <h2 id="lender">${amount.toFixed(2)}</h2>;
+    }
+  };
+
+  const lenderBalance = () => {
+    const lenderExpenses = [];
+    for (let i = 0; i < props.expenses.length; i++) {
+      const expense = props.expenses[i];
+      if (expense.payee_id !== undefined && expense.payee_id === props.logged_in) {
+        lenderExpenses.push(expense);
+      } else if (expense.splits !== undefined && expense.payer_id === props.logged_in) {
+        lenderExpenses.push(expense);
+      }
+    }
+    let amount = balance(lenderExpenses, props.logged_in);
+    return <h2 id="lender">${amount.toFixed(2)}</h2>;
+  };
+
+  return (
+    <div className="dashboard">
+      <ul id="balances">
+        <li>
+          <h1>total balance</h1>
+          {totalBalance()}
+        </li>
+        <li>
+          <h1>you owe</h1>
+          {oweBalance()}
+        </li><li>
+          <h1>you are owed</h1>
+          {lenderBalance()}
+        </li>
+      </ul>
+      <nav id="dashboard-nav">
+        <h1 id="head-left">YOU OWE</h1>
+        <ul>
+          <li id="view-list">
+            <i className="fa fa-bars"></i>
+            view as list
           </li>
-          <li>
-            <h1>you owe</h1>
-          </li><li>
-            <h1>you are owed</h1>
+          <li id="view-chart">
+            <i className="fa fa-bar-chart"></i>
+            view chart
           </li>
         </ul>
-        <nav id="dashboard-nav">
-          <h1 id="head-left">YOU OWE</h1>
-          <ul>
-            <li id="view-list">
-              <i className="fa fa-bars"></i>
-              view as list
-            </li>
-            <li id="view-chart">
-              <i className="fa fa-bar-chart"></i>
-              view chart
-            </li>
-          </ul>
-          <h1 id="head-right">YOU ARE OWED</h1>
-        </nav>
-      </div>
-    );
-  }
+        <h1 id="head-right">YOU ARE OWED</h1>
+      </nav>
+    </div>
+  );
+
 
 };
 
--- frontend/components/dashboard/dashboard_container.js
+++ frontend/components/dashboard/dashboard_container.js
@@ -3,16 +3,16 @@ import { withRouter } from "react-router";
 import Dashboard from "./dashboard";
 
 const mapStateToProps = state => {
+  const bills = Object.values(state.entities.bills);
+  const payments = Object.values(state.entities.payments);
+  let expenses = bills.concat(payments);
   return {
+    expenses: expenses,
+    users: state.entities.users,
     logged_in: state.session.id || false
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-  };
-};
-
-const dashboard = connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+const dashboard = connect(mapStateToProps)(Dashboard);
 
 export default withRouter(dashboard);
